795a776d90fb6d2a36dd3bff1bbe21c809d585f6
Markus Ng <e0014936@u.nus.edu>
2022-05-11 15:40:39 +0800

    Update User Input UI

--- map.py
+++ map.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+from random import randint, random
 
-st.title('Default StreamLit ST GeoMaps')
+
+st.title('Lease Prediction')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -11,30 +13,13 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    DATA_URL = 'https://api.data.gov.sg/v1/transport/taxi-availability'
+    json = requests.get(DATA_URL).json()
+    df = pd.DataFrame(json["features"][0]["geometry"]["coordinates"])
+    df.columns = ['long', 'lat']
+    return df[:nrows]
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+df = load_data(10000)
 
 ############################################################
 ############################################################
@@ -43,57 +28,41 @@ st.map(filtered_data)
 sg_lat = 1.3521
 sg_long = 103.8198
 
-DATA_URL = 'https://api.data.gov.sg/v1/transport/taxi-availability'
-json = requests.get(DATA_URL).json()
-df = pd.DataFrame(json["features"][0]["geometry"]["coordinates"])
-df['third'] = 0
-df.columns = ['long', 'lat', 'extra']
-df['long'] = pd.to_numeric(df.long, errors='coerce')
-df['lat'] = pd.to_numeric(df.lat, errors='coerce')
-st.write(df.head())
+st.header("Explore Lease Prices Here")
+st.dataframe(df)
 
-############################################################
-############################################################
-############################################################
-
-# from streamlit_keplergl import keplergl_static
-# from keplergl import KeplerGl
-
-# st.title('KeplerGl Maps')
-# map_1 = KeplerGl(data={'Data': df})
-# config = {
-# 'version': 'v1',
-# 'config': {
-#     'mapState': {
-#         'latitude': sg_lat,
-#         'longitude': sg_long,
-#         'zoom': 10
-#     	}
-# 	}
-# }
-
-# map_1.config = config
-# map_1.add_data(data=df, name="extra")
-# keplergl_static(map_1)
+st.header("Lease Prices Prediction")
+with st.expander("Input Property Details for Prediction"):
+    location = st.text_input('Postal Code?')
+    bdrm = st.selectbox('Number of Bedroom?',('1Bdrm', '2Bdrm', '3Bdrm', '4Bdrm', '5Bdrm'))
+    prop_type = st.selectbox('Property Type?',('HDB', 'Condo', 'Landed'))
+    age = st.number_input("Age of Property?", 0, 50)
+    
+clicked = st.button("Run Prediction")
+if clicked:
+    st.write('Prediction Completed')
+    predicted = "$ " + "{:,}".format(randint(100000,1000000))
+    delta = str(round(random(),1)) + " %"
+    st.metric('Predicted Lease', predicted, delta = delta, delta_color="normal")
 
 ############################################################
 ############################################################
 ############################################################
 import pydeck as pdk
-st.title('PyDeck Maps')
+st.header("Look at Map Lease Prices")
 st.pydeck_chart(pdk.Deck(
      initial_view_state=pdk.ViewState(
-     	latitude= sg_lat,
+        latitude= sg_lat,
         longitude= sg_long,
         zoom= 10
      ),
-	layers = [pdk.Layer(
-	    'ScreenGridLayer',
-	    df,
-	    get_position=['long', 'lat'],
-	    cell_size_pixels=20,
-	    pickable=True,
-	    auto_highlight=True	    
-		) 
-	]
+    layers = [pdk.Layer(
+        'ScreenGridLayer',
+        df,
+        get_position=['long', 'lat'],
+        cell_size_pixels=20,
+        pickable=True,
+        auto_highlight=True     
+        ) 
+    ]
  ))
